9abaa69d1e6bc5fee3d95d8d1d7ad223bd507da9
mandlm <mandlm@molez.net>
2016-01-12 22:47:20 +0100

    Restructured code into serveral source files

---
+++ LCD/LCD/Pin.cpp
@@ -0,0 +1,31 @@
+#include "Pin.h"
+
+Pin::Pin(volatile uint8_t *port, uint8_t pin) : m_port(port)
+	, m_pin(pin)
+{
+
+}
+
+void Pin::pulse()
+{
+	*m_port |= (1 << m_pin);
+	*m_port &= ~(1 << m_pin);
+}
+
+void Pin::set(bool value)
+{
+	if (value == true)
+	{
+		*m_port |= (1 << m_pin);
+	}
+	else
+	{
+		*m_port &= ~(1 << m_pin);
+	}
+}
+
+void Pin::toggle()
+{
+	*m_port ^= (1 << m_pin);
+}
+
