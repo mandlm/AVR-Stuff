c713fe96f40f66b7f3d14e56f2346d9922b95619
mandlm <mandlm@molez.net>
2016-01-31 15:08:07 +0100

    Output RF signal to led

--- RF433/Receiver/Receiver/main.cpp
+++ RF433/Receiver/Receiver/main.cpp
@@ -1,14 +1,24 @@
 #define F_CPU 12000000
 
 #include <avr/io.h>
-
+#include <util/delay.h>
 
 int main()
 {
-
+	DDRB &= ~(1 << PB0);	// data input
+	DDRD |= (1 << PD6);		// led output
 
 	while (true)
 	{
+		if (PINB & (1 << PB0))
+		{
+			PORTD |= (1 << PD6);
+		}
+		else
+		{
+			PORTD &= ~(1 << PD6);
+		}
+		//_delay_ms(10);
 	}
 }
 
