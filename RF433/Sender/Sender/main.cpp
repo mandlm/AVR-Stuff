29c3daa02998110ff3714f5eaaeacc0f463b1122
mandlm <mandlm@molez.net>
2016-01-31 14:15:01 +0100

    Project setup for a RF433MHz sender

---
+++ RF433/Sender/Sender/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Sender.cpp
+ *
+ * Created: 31.01.2016 14:09:45
+ * Author : mandlm
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
